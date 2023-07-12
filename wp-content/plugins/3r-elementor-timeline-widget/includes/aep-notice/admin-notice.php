4601944e840de3ce74b88c4f74c1abe514590962
Prasad Lakshan <kpdplakshan@gmail.com>
2023-07-12 06:14:23 +0530

    relase

---
+++ wp-content/plugins/3r-elementor-timeline-widget/includes/aep-notice/admin-notice.php
@@ -0,0 +1,5 @@
+<br>
+<b>Fatal error</b>:  Uncaught Error: Call to undefined function get_option() in C:\xampp\htdocs\cssl\wp-content\plugins\3r-elementor-timeline-widget\includes\aep-notice\admin-notice.php:3
+Stack trace:
+#0 {main}
+  thrown in <b>C:\xampp\htdocs\cssl\wp-content\plugins\3r-elementor-timeline-widget\includes\aep-notice\admin-notice.php</b> on line <b>3</b><br>
