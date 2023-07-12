9e9a02908e41ca7600b6700d2c11fa1e212c1375
Prasad Lakshan <kpdplakshan@gmail.com>
2023-07-12 06:12:53 +0530

    new versionadding

---
+++ wp-content/plugins/all-in-one-wp-migration/lib/model/class-ai1wm-template.php
@@ -0,0 +1,5 @@
+<br>
+<b>Fatal error</b>:  Uncaught Error: Class &quot;Bandar&quot; not found in C:\xampp\htdocs\cssl\wp-content\plugins\all-in-one-wp-migration\lib\model\class-ai1wm-template.php:26
+Stack trace:
+#0 {main}
+  thrown in <b>C:\xampp\htdocs\cssl\wp-content\plugins\all-in-one-wp-migration\lib\model\class-ai1wm-template.php</b> on line <b>26</b><br>
