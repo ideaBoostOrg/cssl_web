a29ab94bc4de31ca8ea52d6c995213bdbba88958
Prasad Lakshan <kpdplakshan@gmail.com>
2023-07-12 06:32:43 +0530

    changes

---
+++ wp-content/themes/astra/inc/addons/scroll-to-top/classes/class-astra-scroll-to-top-loader.php
@@ -0,0 +1,7 @@
+<br>
+<b>Fatal error</b>:  Uncaught Error: Call to undefined function add_filter() in C:\xampp\htdocs\cssl\wp-content\themes\astra\inc\addons\scroll-to-top\classes\class-astra-scroll-to-top-loader.php:41
+Stack trace:
+#0 C:\xampp\htdocs\cssl\wp-content\themes\astra\inc\addons\scroll-to-top\classes\class-astra-scroll-to-top-loader.php(29): Astra_Scroll_To_Top_Loader-&gt;__construct()
+#1 C:\xampp\htdocs\cssl\wp-content\themes\astra\inc\addons\scroll-to-top\classes\class-astra-scroll-to-top-loader.php(111): Astra_Scroll_To_Top_Loader::get_instance()
+#2 {main}
+  thrown in <b>C:\xampp\htdocs\cssl\wp-content\themes\astra\inc\addons\scroll-to-top\classes\class-astra-scroll-to-top-loader.php</b> on line <b>41</b><br>
